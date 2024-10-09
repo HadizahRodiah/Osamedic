5e4bdcaf5db22d0e8e84f47e2c0b97336e28322b
HadizahRodiah <123167408+HadizahRodiah@users.noreply.github.com>
2024-10-09 19:54:57 +0100

    Add files via upload

---
+++ src/components/Auth/SignupPage.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import './signup.css';
+import Company from '../../assets/company.png'
+
+function SignupPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:4000/signup', { email, password });
+            alert(response.data); // Use the server's response message
+            navigate('/login');
+        } catch (error) {
+            alert(error.response ? error.response.data : 'Signup failed');
+        }
+    };
+
+    return (
+        <div className="signup-container">
+            <div className="signup-image">
+                <img src={Company} alt="Logo" />
+                <h2>We are The OSAMEDIC Team</h2>
+                <p>We are more than just a company. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="signup">
+                <h1>Sign Up</h1>
+                {/* <form onSubmit={handleSubmit}>
+                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" required />
+                    <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" required />
+                    <button type="submit">Register</button> */}
+                <p>Sign-ups are currently disabled. Please contact support for more information.</p>
+            </div>
+        </div>
+    );
+}
+
+export default SignupPage;
